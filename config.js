8e86b538c140f5a4fb67488b2c7cdbaebf45005f
Ieshalebest <87079142+Ieshalebest@users.noreply.github.com>
2021-07-18 13:44:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || "ry!", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/Gwg2xR2ymF", //Support Server Link
-  Token: process.env.Token || "ODY1NjkxOTQ5MTcyMjYwODkz.YPHsdA.o1iU0NfExtyOZtU7rVR3IBAA2WY", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "865691949172260893", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "2eCZlgP00NtXrxiwo9-svNIfrt56DLSl", //Discord Client Secret
+  Token: process.env.Token || "", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
