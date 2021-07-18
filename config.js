0481b8f22925866ff4818252b238c386e4e9fde7
Ieshalebest <87079142+Ieshalebest@users.noreply.github.com>
2021-07-18 13:41:35 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  Admins: ["UserID", "UserID"], //Admins of the bot
+  Admins: ["277060454549815296", "391944863974948866"], //Admins of the bot
   ExpressServer: true,//If you wanted to make the website run or not
-  DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
+  DefaultPrefix: process.env.Prefix || "ry!", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
-  SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
+  SupportServer: "https://discord.gg/Gwg2xR2ymF", //Support Server Link
+  Token: process.env.Token || "ODY1NjkxOTQ5MTcyMjYwODkz.YPHsdA.o1iU0NfExtyOZtU7rVR3IBAA2WY", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "865691949172260893", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "2eCZlgP00NtXrxiwo9-svNIfrt56DLSl", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
-  CookieSecret: "Pikachu is cute", //A Secret like a password
+  CookieSecret: "rytropremium", //A Secret like a password
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   Permissions: 2205280576, //Bot Inviting Permissions
